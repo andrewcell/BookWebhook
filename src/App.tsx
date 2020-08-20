766f81f941c90c73e899e0eebda96476abb98284
Andrew M. Bray (S.Y. Choe) <a@vxz.me>
2020-08-20 15:41:07 +0900

    Some updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Typography} from "@material-ui/core";
+import { Provider } from 'react-redux';
+import {BrowserRouter, Switch, Route, Link} from 'react-router-dom';
 
-const App = (): JSX.Element => {
-  return (
-      <div className="App">
-        <Typography variant={'h3'}>
-            It works!
-        </Typography>
-      </div>
-  )
+interface AppState {
+
+}
+
+class App extends React.Component<{}, AppState> {
+    constructor(props: {}) {
+        super(props);
+    }
+    render(): React.ReactElement {
+        if (true/* state logged in */) {
+            return (
+                <BrowserRouter>
+                    <Switch>
+                        <Route exact path={'/webui'}>
+                            <div className="App">
+                                <Typography variant={'h3'}>
+                                    It works!
+                                </Typography>
+                                <Link to={'a'}>asd</Link>
+                            </div>
+                        </Route>
+                        <Route path={'/webui/'}>
+                            <h1>A</h1>
+                        </Route>
+                    </Switch>
+                </BrowserRouter>
+            )
+        } else {
+            /* return ( Login Page
+
+            )*/
+        }
+    }
 }
 
 export default App;
